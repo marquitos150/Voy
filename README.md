5540dfc861d8376406d46266496e3c3b4391aeb2
marquitos150 <mmvazquez364@gmail.com>
2024-12-19 19:11:32 -0500

    Refined implementation; added TODOs and labels

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Voy
 for honors thesis
+
+## TODO
+0. Start implementing a program that can classify a thumbs up or not
+1. Classify novel hand gesture interaction techniques for moving and rotating in a VE (forward, backward, left, right)
+2. For training a model, you need to create a training and validation set. Make a dataset which contains videos of friends/family performing gestures.
+3. Feed the videos into the machine for the model to be trained on that dataset.
+4. Implement a model and use mediapipe (e.g., CNN and mediapipe) that can detect and classify these hand gestures from hand landmarks (static/dynamic) (e.g., adjusting speed for dynamic hand gestures)
+5. Ask friends to do something and see how the model can detect and classify the hand gesture.
+6. After finishing the training, you can do parameter comparison or use other model to increase the accuracy.
+
--- app.py
+++ app.py
@@ -1,25 +1,75 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import argparse # custom arguments
+import csv # data collection
+
 import mediapipe as mp
-import cv2
-import numpy
+import cv2 as cv
+import numpy as np
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mphands = mp.solutions.hands
+def get_args():
+    parser = argparse.ArgumentParser()
 
-cap = cv2.VideoCapture(0)
-hands = mphands.Hands()
-while True:
-    data, image = cap.read()
-    # Flip the image
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # Storing the results
-    results = hands.process(image)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                image,
+    parser.add_argument("--width", help='Enter width as an integer', type=int, default=960)
+    parser.add_argument("--height", help='Enter height as an integer', type=int, default=540)
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    # Grab arguments for parsing
+    args = get_args()
+    
+    # Prepare the camera
+    cap = cv.VideoCapture(0)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, args.width)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, args.height)
+
+    # Setup the mediapipe for hand detection
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    hands = mp_hands.Hands(
+        static_image_mode = False, # needed for processing a continuous stream of images
+        max_num_hands = 2, # one hand for movement; another hand for rotation
+                           # or it can be two hands for movement and rotation 
+    )
+
+    # Collect the classification labels
+    with open('./navigation_labels.csv', encoding='utf-8-sig') as f:
+        navigation_labels = csv.reader(f)
+        navigation_labels_list = [row[0] for row in navigation_labels]
+
+    # Continuously run gesture recognition system
+    while True:
+        # Exit case
+        key = cv.waitKey(10)
+        if key == 27:  # ESC
+            break
+
+        # Camera capture of a frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv.flip(frame, 1) # Flip or mirror the display
+        
+        # Mark the image as not writeable for improved performance and process the RGB image to detect hands
+        frame.flags.writeable = False
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        results = hands.process(frame)
+
+        # After processing, draw landmarks on the frame, requiring it to be modifiable
+        frame.flags.writeable = True
+        frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+
+        if results.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+                mp_drawing.draw_landmarks(
+                frame,
                 hand_landmarks,
-                mphands.HAND_CONNECTIONS)
-    cv2.imshow('Handtracker', image)
-    cv2.waitKey(1)
+                mp_hands.HAND_CONNECTIONS)
+        cv.imshow('Handtracker', frame)
+        cv.waitKey(1)
+
+if __name__ == '__main__':
+    main()
